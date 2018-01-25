229853f4ca46952a4fe8dfda555d736ed90120f2
Ivo Houbrechts <ivo.houbrechts@nike.com>
2018-01-25 17:01:53 +0100

    working api gateway

--- src/main/java/be/houbrechtsit/goserverless/MovieAPILambda.java
+++ src/main/java/be/houbrechtsit/goserverless/MovieAPILambda.java
@@ -2,12 +2,31 @@ package be.houbrechtsit.goserverless;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.google.gson.Gson;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MovieAPILambda {
 
-    private MovieRepository movieRepository = MovieRepository.geInstance();
+//    private MovieRepository movieRepository = MovieRepository.geInstance();
 
-    protected String handleApiGatewayRequest(Object request, Context context) {
-        return new Gson().toJson(new Movie("Starwars", "George Lucas", 1978));
+    public ProxyResponse handleRequest(Object request, Context context) {
+        Map<String, Object> dump = new HashMap<>();
+        dump.put("request", request);
+        dump.put("request-class", request.getClass().getName());
+        dump.put("context", context);
+        return new ProxyResponse(200, Collections.emptyMap(), new Gson().toJson(dump));
+    }
+
+
+    @Data
+    @AllArgsConstructor
+    public class ProxyResponse {
+        private int statusCode;
+        private Map<String, Object> headers;
+        private String body;
     }
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,33 @@ provider "aws" {
   region = "${var.region}"
 }
 
+resource "aws_api_gateway_rest_api" "movie_api" {
+  name = "movie_api"
+  description = "Sample movies REST API"
+}
+
+resource "aws_api_gateway_resource" "movies_resource" {
+  rest_api_id = "${aws_api_gateway_rest_api.movie_api.id}"
+  parent_id = "${aws_api_gateway_rest_api.movie_api.root_resource_id}"
+  path_part = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "any_method" {
+  rest_api_id = "${aws_api_gateway_rest_api.movie_api.id}"
+  resource_id = "${aws_api_gateway_resource.movies_resource.id}"
+  http_method = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "api_method_integration" {
+  rest_api_id = "${aws_api_gateway_rest_api.movie_api.id}"
+  resource_id = "${aws_api_gateway_resource.movies_resource.id}"
+  http_method = "${aws_api_gateway_method.any_method.http_method}"
+  type = "AWS_PROXY"
+  uri = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.movie_api_lambda.arn}/invocations"
+  integration_http_method = "POST"
+}
+
 resource "aws_iam_role" "lambda_exec_role" {
   name = "lambda_exec_role"
   assume_role_policy = <<EOF
@@ -30,25 +57,25 @@ resource "aws_lambda_function" "movie_api_lambda" {
   filename = "../build/distributions/go-serverless-example-0.1.zip"
 
   function_name = "movie_api_lambda"
-  role = "arn:aws:iam::069272765570:role/carrierint-lambda-systemtesting"
-  handler = "be.houbrechtsit.goserverless.MovieAPILambda:handleApiGatewayRequest"
+  role = "${aws_iam_role.lambda_exec_role.arn}"
+  handler = "be.houbrechtsit.goserverless.MovieAPILambda::handleRequest"
   runtime = "java8"
-  publish = true
-  memory_size = "1024"
-  timeout = "20"
-
-  environment {
-    variables = {
-      TABLE_REGION = "${var.region}"
-      TABLE_NAME_PREFIX = "test-"
-      requestType = "json"
-    }
-  }
 }
+
 resource "aws_lambda_permission" "api_lambda_permissions" {
   statement_id = "AllowExecutionFromAPIGateway"
   action = "lambda:InvokeFunction"
   function_name = "movie_api_lambda"
   principal = "apigateway.amazonaws.com"
-  source_arn = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${var.gateway_api_id}/${var.gateway_stage_name}/*/*"
+}
+
+resource "aws_api_gateway_deployment" "apig_deployment" {
+  rest_api_id = "${aws_api_gateway_rest_api.movie_api.id}"
+  stage_name = "test"
+  stage_description = "test"
+  depends_on = ["aws_api_gateway_integration.api_method_integration"]
+}
+
+output "display_invoke_url" {
+  value = "Invoke URL: ${aws_api_gateway_deployment.apig_deployment.invoke_url}"
 }
