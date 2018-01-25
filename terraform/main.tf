8e5cfeec6c357c99681b9faba4f5532e22721291
Ivo Houbrechts <ivo.houbrechts@nike.com>
2018-01-25 18:04:20 +0100

    swagger based api gateway

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,29 +9,17 @@ provider "aws" {
 resource "aws_api_gateway_rest_api" "movie_api" {
   name = "movie_api"
   description = "Sample movies REST API"
+  body = "${data.template_file.swagger.rendered}"
 }
 
-resource "aws_api_gateway_resource" "movies_resource" {
-  rest_api_id = "${aws_api_gateway_rest_api.movie_api.id}"
-  parent_id = "${aws_api_gateway_rest_api.movie_api.root_resource_id}"
-  path_part = "{proxy+}"
+data "template_file" "swagger" {
+  template = "${file("../swagger.yaml")}"
+
+  vars {
+    invocation_uri = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.movie_api_lambda.arn}/invocations"
+  }
 }
 
-resource "aws_api_gateway_method" "any_method" {
-  rest_api_id = "${aws_api_gateway_rest_api.movie_api.id}"
-  resource_id = "${aws_api_gateway_resource.movies_resource.id}"
-  http_method = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "api_method_integration" {
-  rest_api_id = "${aws_api_gateway_rest_api.movie_api.id}"
-  resource_id = "${aws_api_gateway_resource.movies_resource.id}"
-  http_method = "${aws_api_gateway_method.any_method.http_method}"
-  type = "AWS_PROXY"
-  uri = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.movie_api_lambda.arn}/invocations"
-  integration_http_method = "POST"
-}
 
 resource "aws_iam_role" "lambda_exec_role" {
   name = "lambda_exec_role"
@@ -65,7 +53,7 @@ resource "aws_lambda_function" "movie_api_lambda" {
 resource "aws_lambda_permission" "api_lambda_permissions" {
   statement_id = "AllowExecutionFromAPIGateway"
   action = "lambda:InvokeFunction"
-  function_name = "movie_api_lambda"
+  function_name = "${aws_lambda_function.movie_api_lambda.function_name}"
   principal = "apigateway.amazonaws.com"
 }
 
@@ -73,9 +61,13 @@ resource "aws_api_gateway_deployment" "apig_deployment" {
   rest_api_id = "${aws_api_gateway_rest_api.movie_api.id}"
   stage_name = "test"
   stage_description = "test"
-  depends_on = ["aws_api_gateway_integration.api_method_integration"]
 }
 
 output "display_invoke_url" {
   value = "Invoke URL: ${aws_api_gateway_deployment.apig_deployment.invoke_url}"
 }
+
+output "invocation_uri" {
+  value = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.movie_api_lambda.arn}/invocations"
+}
+
