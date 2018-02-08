cb1c71b604c339fa3e3bf5d6e5e249717aa2fdbc
Ivo Houbrechts <ivo.houbrechts@nike.com>
2018-02-08 13:22:00 +0100

    functional info endpoint

--- src/main/java/be/houbrechtsit/awsserverless/MovieAPILambda.java
+++ src/main/java/be/houbrechtsit/awsserverless/MovieAPILambda.java
@@ -25,81 +25,13 @@ public class MovieAPILambda {
     public static final String MOVIE_ID_PATH_PARAMETER = "movieId";
     public static final String PATH_PARAMETERS = "pathParameters";
 
-    private MovieRepository movieRepository = MovieRepository.geInstance();
 
     public ProxyResponse handleRequest(Map<String, Object> request, Context context) {
-        LambdaLogger logger= context.getLogger();
-        logger.log("request: " +  new Gson().toJson(request));
-        logger.log("context: " +  new Gson().toJson(context));
+        LambdaLogger logger = context.getLogger();
+        logger.log("request: " + new Gson().toJson(request));
+        logger.log("context: " + new Gson().toJson(context));
 
-        String resource = (String) request.get("resource");
-        if ("/v1/movies".equals(resource)) {
-            return handleMovieRequest(request, context);
-        }
-        if ("/v1/info".equals(resource)) {
-            return handleInfoRequest(request, context);
-        }
-        return new ProxyResponse(404, null, null);
-    }
-
-    private ProxyResponse handleMovieRequest(Map<String, Object> request, Context context) {
-        String movieId = getMovieId(request);
-        String httpMethod = (String) request.get(HTTP_METHOD);
-        String body = (String) request.get(BODY);
-
-        if (movieId == null) {
-            if (GET.equals(httpMethod)) {
-                return getMovies();
-            } else if (POST.equals(httpMethod)) {
-                return createMovie(gson.fromJson(body, Movie.class));
-            }
-            return methodNotAllowed();
-        } else {
-            if (GET.equals(httpMethod)) {
-                return getMovie(movieId);
-            } else if (PUT.equals(httpMethod)) {
-                return updateMovie(movieId, gson.fromJson(body, Movie.class));
-            } else if (DELETE.equals(httpMethod)) {
-                return deleteMovie(movieId);
-            }
-            return methodNotAllowed();
-        }
-    }
-
-    private ProxyResponse getMovies() {
-        return new ProxyResponse(200, null, gson.toJson(movieRepository.list()));
-    }
-
-    private ProxyResponse createMovie(Movie movie) {
-        movieRepository.saveOrUpdate(movie);
-        return new ProxyResponse(200, null, gson.toJson(movie));
-    }
-
-    private ProxyResponse getMovie(String resourceId) {
-        return new ProxyResponse(200, null, gson.toJson(movieRepository.load(resourceId)));
-    }
-
-    private ProxyResponse updateMovie(String resourceId, Movie movie) {
-        movie.setTitle(resourceId);
-        movieRepository.saveOrUpdate(movie);
-        return new ProxyResponse(200, null, gson.toJson(movie));
-    }
-
-    private ProxyResponse deleteMovie(String resourceId) {
-        movieRepository.delete(new Movie(resourceId, null, 0));
-        return new ProxyResponse(204, null, null);
-    }
-
-    private ProxyResponse methodNotAllowed() {
-        return new ProxyResponse(405, null, "{\"error\":\"method not allowed\"}");
-    }
-
-    private String getMovieId(Map<String, Object> request) {
-        Map<String, String> pathParameters = (Map<String, String>) request.get(PATH_PARAMETERS);
-        if (pathParameters != null) {
-            return pathParameters.get(MOVIE_ID_PATH_PARAMETER);
-        }
-        return null;
+        return handleInfoRequest(request, context);
     }
 
     private ProxyResponse handleInfoRequest(Map<String, Object> request, Context context) {
