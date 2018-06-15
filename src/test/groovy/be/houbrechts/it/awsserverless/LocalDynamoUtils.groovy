d2381cc6a417b9d621d418b03c1a040256d9d202
houbie <ivo@houbrechts-it.be>
2018-06-16 01:01:24 +0200

    sam local with local dynamo db

--- src/test/groovy/be/houbrechts/it/awsserverless/LocalDynamoUtils.groovy
+++ src/test/groovy/be/houbrechts/it/awsserverless/LocalDynamoUtils.groovy
@@ -5,70 +5,53 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression
 import com.amazonaws.services.dynamodbv2.document.DynamoDB
 import com.amazonaws.services.dynamodbv2.local.main.ServerRunner
-import com.amazonaws.services.dynamodbv2.model.CreateTableRequest
-import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput
-
-import static com.amazonaws.services.dynamodbv2.util.TableUtils.createTableIfNotExists
 
 /**
  * @author IHoubr
  */
-public class LocalDynamoUtils {
-    private final DynamoDB dynamoDB
-    private final DynamoDBMapper dynamoDBMapper
-    private final AmazonDynamoDB amazonDynamoDB
+class LocalDynamoUtils {
+    private DynamoDB dynamoDB
+    private DynamoDBMapper dynamoDBMapper
+    private AmazonDynamoDB amazonDynamoDB
 
     private static LocalDynamoUtils instance
     private static int port
     private static boolean started = false
 
-    public static LocalDynamoUtils getInstance() {
+    static LocalDynamoUtils getInstance() {
         if (instance != null) {
             return instance
         }
 
-        def socket = new ServerSocket(0)
-        port = socket.getLocalPort()
-        socket.close()
-
-        def movieRepository = MovieRepository.getInstance("http://localhost:$port")
-        instance = new LocalDynamoUtils(movieRepository.amazonDynamoDB, movieRepository.dynamoDBMapper)
+        instance = new LocalDynamoUtils()
         return instance
     }
 
-    public void init() {
-
-    }
-
-    public void startDynamo() {
-        if (!started) {
-            try {
-                ServerRunner.main("-inMemory", "-port", port.toString())
-                started = true
-            } catch (Exception e) {
-                e.printStackTrace()
-            }
+    void init() {
+        if (!port) {
+            def socket = new ServerSocket(0)
+            port = socket.getLocalPort()
+            socket.close()
         }
+
+        try {
+            ServerRunner.main("-inMemory", "-port", port.toString())
+            started = true
+        } catch (Exception e) {
+            e.printStackTrace()
+        }
+
+        def movieRepository = MovieRepository.getInstance("http://localhost:$port")
+        this.dynamoDB = new DynamoDB(movieRepository.amazonDynamoDB)
+        this.dynamoDBMapper = movieRepository.dynamoDBMapper
+        this.amazonDynamoDB = movieRepository.amazonDynamoDB
     }
 
-    private LocalDynamoUtils(AmazonDynamoDB amazonDynamoDB, DynamoDBMapper dynamoDBMapper) {
-        this.dynamoDB = new DynamoDB(amazonDynamoDB)
-        this.dynamoDBMapper = dynamoDBMapper
-        this.amazonDynamoDB = amazonDynamoDB
+    private LocalDynamoUtils() {
     }
-
-    public void createTable(Class persistentClass) {
-        createTableIfNotExists(amazonDynamoDB, createTableRequest(persistentClass))
-    }
-
-    private CreateTableRequest createTableRequest(Class persistentClass) {
-        return dynamoDBMapper.generateCreateTableRequest(persistentClass)
-                .withProvisionedThroughput(new ProvisionedThroughput(1000L, 1000L))
-    }
-
 
     @SuppressWarnings("unchecked")
-    public void clearTable(Class persistentClass) {
+    void clearTable(Class persistentClass) {
         dynamoDBMapper.scan(persistentClass, new DynamoDBScanExpression()).each { dynamoDBMapper.delete(it) }
     }
 }
